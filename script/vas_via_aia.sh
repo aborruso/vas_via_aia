4266d30f81bff8590782467b1baaae5d165a3de7
aborruso <aborruso@gmail.com>
2024-03-07 11:03:12 +0100

    close #4

--- script/vas_via_aia.sh
+++ script/vas_via_aia.sh
@@ -8,6 +8,7 @@ set -o pipefail
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "${folder}"/../data/
+mkdir -p "${folder}"/../data/risorse
 mkdir -p "${folder}"/tmp
 
 # clean up
@@ -24,3 +25,11 @@ mlrgo -I -S --csv sub -f regioni "Valle.+" "Valle d'Aosta" "${folder}"/../data/p
 # dati per numero di progetti presentati per ciascuna regione
 
 mlrgo -S --csv cut -f id,regioni,tipologia then nest --evar "," -f regioni then clean-whitespace then count-distinct -f regioni,tipologia then sort -f regioni,tipologia "${folder}"/../data/progetti_italia.csv > "${folder}"/../data/progetti_italia_regioni.csv
+
+# estrai nomi regioni, province e comuni
+
+mlrgo -S --csv cut -o -f regioni,province,comuni,regioni then uniq -a then \
+nest --evar "," -f regioni then clean-whitespace then uniq -a then \
+nest --evar "," -f province then clean-whitespace then uniq -a then \
+nest --evar "," -f comuni then clean-whitespace then uniq -a then \
+sort -f regioni,province,comuni "${folder}"/../data/progetti_italia.csv >"${folder}"/../data/risorse/luoghi.csv
